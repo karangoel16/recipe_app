0bd14f55ae4dfe0524d25b89de65c57dee8aa7c6
karangoel16 <karangoel16@yahoo.com>
2017-07-05 15:47:55 +0530

    value edit added

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipes.model';
 import {ActivatedRoute} from '@angular/router';
 import {RecipeService} from '../recipe.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html'
@@ -9,6 +10,8 @@ import {RecipeService} from '../recipe.service';
 export class RecipeEditComponent implements OnInit {
   recipe: Recipe;
   id: number;
+  myForm: FormGroup;
+  imagepath: string;
 
   constructor(private activeRoute: ActivatedRoute, private recipeService: RecipeService) {
   }
@@ -16,5 +19,15 @@ export class RecipeEditComponent implements OnInit {
   ngOnInit() {
     this.id = this.activeRoute.snapshot.params['id'];
     this.recipe = this.recipeService.getrecipeById(this.id);
+    this.imagepath = this.recipe.imagePath;
+    this.myForm = new FormGroup({
+      name: new FormControl(this.recipe.name, Validators.required),
+      desc: new FormControl(this.recipe.desc, Validators.required),
+      imagesrc: new FormControl(this.recipe.imagePath, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log('we are here');
   }
 }
